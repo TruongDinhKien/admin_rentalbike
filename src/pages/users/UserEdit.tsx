52e9665d22ddfc20801ccd3a1711fa13bfd53be3
Trương Đình Kiên <truongdinhkien321@gmail.com>
2023-05-26 09:59:35 +0700

    add upload image to server

--- src/pages/users/UserEdit.tsx
+++ src/pages/users/UserEdit.tsx
@@ -1,5 +1,7 @@
 import { ActionButton } from '@/components'
+import ImageUpload from '@/components/ImageUpload'
 import _ from 'lodash'
+import {  useState } from 'react'
 import { Edit, SimpleForm, TextInput, required } from 'react-admin'
 
 export const UserEdit = () => {
@@ -9,16 +11,21 @@ export const UserEdit = () => {
     value = { ...value }
     return value
   }
+  const [url, setUrl] = useState('')
+  const [loading, setLoading] = useState(false)
 
   return (
-    <Edit transform={transformData} actions={<ActionButton />}>
-      <SimpleForm>
-        <TextInput source="email" validate={required()} />
-        <TextInput source="phoneNumber" />
-        <TextInput source="firstName" />
-        <TextInput source="lastName" />
-        <TextInput source="roleName" />
-      </SimpleForm>
-    </Edit>
+    <div>
+      <Edit transform={transformData} actions={<ActionButton />}>
+        <SimpleForm>
+          <TextInput source="email" validate={required()} />
+          <TextInput source="phoneNumber" />
+          <TextInput source="firstName" />
+          <TextInput source="lastName" />
+          <TextInput source="roleName" />
+        </SimpleForm>
+      </Edit>
+      <ImageUpload url={url} setUrl={setUrl} loading={loading} setLoading={setLoading}></ImageUpload>
+    </div>
   )
 }
